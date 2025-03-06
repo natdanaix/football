72b1657910a379d14109897e9f4a0ad7502329ab
natdanaix <103471625+natdanaix@users.noreply.github.com>
2025-03-06 08:18:55 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Football Match Tracker - Main Entry Point
+ * This file imports and initializes the main application components
+ */
+
+// Import required modules
+import './football-tracker.js';
+
+// The application is initialized in the football-tracker.js file
+console.log('Football Match Tracker application started');
